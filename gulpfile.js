3e582afd7ef27915d539338c6bb07bc5a0c0e4b5
denys <voznyukden23@gmail.com>
2018-09-07 15:55:36 +0300

    remove istanbul

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp');
-var istanbul = require('gulp-istanbul');
+// var istanbul = require('gulp-istanbul');
 // We'll use mocha in this example, but any test framework will work
 var mocha = require('gulp-mocha');
 var debug = require('gulp-debug');
 
-gulp.task('pre-test', function () {
-  return gulp.src(['app/**/*.js','!app/tests/**/*.spec.js'])
+/*gulp.task('pre-test', function () {
+  return gulp.src(['app/!**!/!*.js','!app/tests/!**!/!*.spec.js'])
     // Covering files
     .pipe(istanbul())
     // Force `require` to return covered files
     .pipe(istanbul.hookRequire());
-});
+});*/
 
-gulp.task('test', ['pre-test'], function () {
+gulp.task('test', function () {
   console.log('test task')
   return gulp.src(['app/tests/*.js'])
   	// .pipe(debug({title: 'unicorn:'}))
